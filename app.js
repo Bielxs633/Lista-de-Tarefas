5701daaf204446b4fd4f3d61325830791c83778c
Gabriel Soares <161614457+Bielxs633@users.noreply.github.com>
2024-12-19 01:39:25 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,38 +1,73 @@
-/**
- * 
- *  Objetivo: Listar tarefas
- *  Data: 08/10/24
- *  Autor: Gabriel
- *  Versão: 1.0
- * 
- **/
+'use strict';
 
-let botao = document.getElementById('botao')
-let input = document.getElementById('input=tarefa')
-let tarefas = document.getElementById('tarefas')
+let banco = [];
 
-botao.addEventListener('click', function() {
+const getBanco = () => JSON.parse(localStorage.getItem ('todoList')) ?? [];
+const setBanco = (banco) => localStorage.setItem ('todoList', JSON.stringify(banco));
 
-    if (input.value != ''){
-        var tarefa = document.createElement('p')
-        tarefas.appendChild(tarefa)
-        tarefa.innerText = '- ' + input.value
-        input.value = ''
-        tarefa.addEventListener('click', function() {
+const criarItem = (tarefa, status, indice) => {
+    const item = document.createElement('label');
+    item.classList.add('todo__item');
+    item.innerHTML = `
+        <input type="checkbox" ${status} data-indice=${indice}>
+        <div>${tarefa}</div>
+        <input type="button" value="X" data-indice=${indice}>
+    `;
+    document.getElementById('todoList').appendChild(item);
+}
 
-            if (marcado == false) {
-                tarefa.style.textDecoration = 'line-through'
-                marcado = true
-            }
-
-            else{
-                tarefa.style.textDecoration = ''
-                marcado = false
-            }
-        })
-
-        tarefa.addEventListener('dblclick', function() {
-            tarefas;this.removeChild(tarefa)
-        })
+const limparTarefas = () => {
+    const todoList = document.getElementById('todoList');
+    while (todoList.firstChild) {
+        todoList.removeChild(todoList.lastChild);
     }
-})
+}
+
+const atualizarTela = () => {
+    limparTarefas();
+    const banco = getBanco(); 
+    banco.forEach ( (item, indice) => criarItem (item.tarefa, item.status, indice));
+}
+
+const inserirItem = (evento) => {
+    const tecla = evento.key;
+    const texto = evento.target.value;
+    if (tecla === 'Enter'){
+        const banco = getBanco();
+        banco.push ({'tarefa': texto, 'status': ''});
+        setBanco(banco);
+        atualizarTela();
+        evento.target.value = '';
+    }
+}
+
+const removerItem = (indice) => {
+    const banco = getBanco();
+    banco.splice (indice, 1);
+    setBanco(banco);
+    atualizarTela();
+}
+
+const atualizarItem = (indice) => {
+    const banco = getBanco();
+    banco[indice].status = banco[indice].status === '' ? 'checked' : '';
+    setBanco(banco);
+    atualizarTela();
+}
+
+const clickItem = (evento) => {
+    const elemento = evento.target;
+    console.log (elemento.type);
+    if (elemento.type === 'button') {
+        const indice = elemento.dataset.indice;
+        removerItem (indice);
+    }else if (elemento.type === 'checkbox') {
+        const indice = elemento.dataset.indice;
+        atualizarItem (indice);
+    }
+}
+
+document.getElementById('newItem').addEventListener('keypress', inserirItem);
+document.getElementById('todoList').addEventListener('click', clickItem);
+
+atualizarTela();
